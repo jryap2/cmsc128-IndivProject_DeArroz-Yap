e3a7c43f3fce68ce9f57b5b3917e4134e668caff
Johann Ross Yap <jryap2@up.edu.ph>
2025-09-21 01:49:16 +0800

    changed connection string

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb+srv://dbUser:<db_password>@cluster0.t6kb4u4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
+mongoose.connect("mongodb+srv://dbUser:bumblebee23@cluster0.t6kb4u4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
 
 const TaskSchema = new mongoose.Schema({
   title: String,
