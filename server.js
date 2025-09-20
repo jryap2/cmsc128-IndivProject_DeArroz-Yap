8cce88f9e042586e3cd1ba35a08afe8670fca688
Johann Ross Yap <jryap2@up.edu.ph>
2025-09-21 01:10:45 +0800

    edited Port code

--- server.js
+++ server.js
@@ -28,4 +28,5 @@ app.post("/tasks", async (req, res) => {
   res.json(task);
 });
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;  
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
