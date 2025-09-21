479912397102e2109d4168309545cd0e26908603
Johann Ross Yap <jryap2@up.edu.ph>
2025-09-21 16:09:48 +0800

    edited express

--- server.js
+++ server.js
@@ -52,7 +52,7 @@ app.delete("/tasks/:id", async (req, res) => {
     }
 });
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static('public'));
 
 app.use((req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
