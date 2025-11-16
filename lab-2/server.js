0c1b6b227c89a9382c76a88f2b65423c22a6a2d2
Johann Ross Yap <jryap2@up.edu.ph>
2025-11-16 13:05:40 +0800

    Brought over my change to server.js

--- lab-2/server.js
+++ lab-2/server.js
@@ -90,5 +90,6 @@ app.use((req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+// Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
